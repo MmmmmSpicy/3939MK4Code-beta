ed4d4945fabc041737a7285e1a9af13156421dc6
MmmmmSpicy <ethanspickard@gmail.com>
2023-12-23 13:11:30 -0500

    Changed depricated Joystick input

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
@@ -62,7 +63,7 @@ public class SwerveSubsystem extends SubsystemBase {
         }).start();
     }
 
-    public void zeroHeading() {
+    public Command zeroHeading() {
         gyro.reset();
     }
 
